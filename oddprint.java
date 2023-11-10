b52612bd87a0e786ae695e5721664e606a3570a0
rameshreddy <jaikumarrock55@gmail.com>
2023-11-10 13:06:36 +0530

    hello

---
+++ oddprint.java
@@ -0,0 +1,13 @@
+class oddprint
+{
+                public static void main(String args[ ]){
+                                    int num=0;
+                                    while(num<10)
+                 {
+                                   ++ num;
+                                    if(num%2==0)
+                                    break;
+                                      System.out.println("odd="+num);
+                   }
+                   }
+                   }
